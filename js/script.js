168f7bf6f5f18759eab67aec05518c57822104c5
hituhin <hituhin09@gmail.com>
2020-11-24 23:22:49 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,129 @@
+$(window).on("load", function(){
+ $(".loader").fadeOut(1000, function(){
+  $("loader").feadout(750);
+ });
+});
+
+
+$(document).ready(function(){
+  $('#slides').superslides({
+    animation: 'fade',
+     play: 300,
+    pagination: false
+  });
+  var typed = new Typed(".typed",{
+     strings: ["Software Engineer.","JavaScript developer.","IoT Engineer.","Student"],
+     typeSpeed: 100,
+     loop: true,
+     startDisplay: 1000,
+     showCursor: false
+
+  });
+
+ $('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+         938:{
+            items:4
+        }
+    }
+  });
+   
+
+   var skillsTopOffset = $(".skillsSection").offset().top;
+   var startsTopOffset = $(".statsSection").offset().top;
+   var countUpFinished = false; 
+
+   $(window).scroll(function(){
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200 ) {
+        $('.chart').easyPieChart({
+              easing: 'easeInOut',
+              barColor:'#fff',
+              trackColor: false,
+              scaleColor: false,
+              lineWidth: 4,
+              size: 152,
+              onStep: function(from, to, percent){
+                $(this.el).find('.percent').text(Math.round(percent));
+              }
+
+          });
+        }
+
+        if (!countUpFinished && window.pageYOffset > startsTopOffset - $(window).height() + 200 ) {
+              $(".counter").each(function(){
+                var element = $(this);
+                var endVal = parseInt(element.text());
+                 element.countup(endVal);
+                 });
+              countUpFinished = true;
+          }
+     });
+   
+   $("[data-fancybox]").fancybox();  
+    
+    $(".items").isotope({
+      filter: '*',
+      animationOptions: {
+        duration: 1500,
+        easing: 'liner',
+        queue: false
+      }
+    }) ;
+
+    $("#filters a").click(function(){
+      $("#filters .current").removeClass("current");
+      $(this).addClass("current");
+      var selector = $(this).attr("data-filter");
+            $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+              duration: 1500,
+              easing: 'liner',
+              queue: false
+            }
+         }) ;
+     return false;
+      });
+
+    $("#navigation li a").click(function(e){
+      e.preventDefault();
+
+      var targetElement = $(this).attr("href");
+      var targetPosition = $(targetElement).offset().top;
+      $("html, body").animate({ scrollTop: targetPosition - 50}, "slow");
+
+
+    });
+
+    const nav = $("#navigation");
+    var navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation(){
+
+      var body = $("body");
+
+      if ($(window).scrollTop() >= navTop) {
+       body.css("padding-top", nav.outerHeight() + "px");
+        body.addClass("fixedNav");
+      }
+      else{
+        body.css("padding-top", 0);
+        body.removeClass("fixedNav");
+      }
+    }
+
+
+ });
